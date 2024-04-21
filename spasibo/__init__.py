4046ca9138ba81ce0570b4c8174978a0cb7681d7
Лиза <104992063+olegoloo@users.noreply.github.com>
2024-04-21 20:39:50 +0300

    Update __init__.py

--- spasibo/__init__.py
+++ spasibo/__init__.py
@@ -22,19 +22,26 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
 
-   
+    get = models.StringField(label='Как Вы хотите распорядиться своим выигрышем?',
+                             choices=['Забрать выигрыш',
+                                      'Сделать пожертвование в благотворительный фонд "Подари жизнь"'],
+                             widget=widgets.RadioSelect)
+    contact = models.StringField(
+        label='Если вы хотите получить выигрыш, оставьте свой контакт тут, с вами свяжутся для уточнения деталей')
 
+
+# PAGES
 class WinnerPage(Page):
-    form_model = 'player'
     def is_displayed(self):
         return self.id_in_group == 1 or self.id_in_group == 8
-    
-    
-class NoWinPage(Page):
     form_model = 'player'
+    form_fields = ['get', 'contact']
+
+class NoWinPage(Page):
     def is_displayed(self):
         return self.id_in_group != 1 and self.id_in_group != 8
-    
+    form_model = 'player'
+
 
 
 page_sequence = [WinnerPage, NoWinPage]
